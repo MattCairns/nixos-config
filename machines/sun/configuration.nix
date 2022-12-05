a0e29220bcfb6c5a1ca17ea1a9ee9580b121d607
Matthew Cairns <mattrcairns@gmail.com>
2022-12-04 20:15:16 -0800

    feat: add sun

---
+++ machines/sun/configuration.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [ 
+    /etc/nixos/hardware-configuration.nix
+      ../../config/base.nix
+      ../../config/users.nix
+      ../../pkgs/essentials.nix
+      ../../pkgs/essentials.nix
+      ../../pkgs/dev.nix
+      ../../pkgs/neovim.nix
+      ../../pkgs/bspwm.nix
+    ];
+
+  networking.hostName = "sun"; 
+
+# Enable touchpad support 
+  services.xserver.libinput.enable = true;
+}
