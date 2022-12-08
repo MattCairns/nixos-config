a94c0eceaf2de83eaff3964865f5ed6321a5e701
Matthew Cairns <mattrcairns@gmail.com>
2022-12-07 17:56:54 -0800

    feat: add kernel modules for thinkpad

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -13,6 +13,17 @@
 
 # Enable touchpad support 
   services.xserver.libinput.enable = true;
+# Enable TLP
+  /* services.tlp.enable = true; */
+# Fingerprint
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = true;
+  security.pam.services.xscreensaver.fprintAuth = true;
+# Kernel mods optimize thinkpad
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "thinkpad_acpi" ];
+  boot.initrd.kernelModules = [ "acpi_call" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
   nix = {
   	package = pkgs.nixFlakes;
