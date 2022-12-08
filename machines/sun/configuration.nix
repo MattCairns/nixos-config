247fcae65c86c81d1b6f4bb6062d49b34e47a7ca
Matthew Cairns <mattrcairns@gmail.com>
2022-12-07 16:52:16 -0800

    feat: use open source nvidia kernel drivers

--- machines/sun/configuration.nix
+++ machines/sun/configuration.nix
@@ -15,7 +15,10 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    open = true;
+  };
 
   nix = {
   	package = pkgs.nixFlakes;
