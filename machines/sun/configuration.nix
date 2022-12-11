2bfa7baf4f9fa29dfe51114f297adcf5a17a1cee
Matthew Cairns <mattrcairns@gmail.com>
2022-12-11 14:20:50 -0800

    fix: use opengl

--- machines/sun/configuration.nix
+++ machines/sun/configuration.nix
@@ -14,7 +14,15 @@
   services.xserver.libinput.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.beta;
     open = true;
