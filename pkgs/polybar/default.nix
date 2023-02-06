da166940d4e0a46a4542f83c15587b02c9c92f24
Matthew Cairns <mattrcairns@gmail.com>
2023-02-06 10:22:46 -0800

    fix: install polybarFull so that sound display works

--- pkgs/polybar/default.nix
+++ pkgs/polybar/default.nix
@@ -2,15 +2,10 @@
 , pkgs
 , ...
 }: {
-  services.polybar = {
-    enable = true;
-    script =
-      ''
-        #!/usr/bin/env sh
-        for m in $(polybar --list-monitors | cut -d":" -f1); do
-            MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybar-$m.log & disown
-        done
-      '';
-  };
+
+  home.packages = with pkgs; [
+    polybarFull
+  ];
+
   xdg.configFile."polybar".source = ./config;
 }
