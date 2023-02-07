d73c19880393d386561ca702c9f6db049acf3d5b
Matthew Cairns <mattrcairns@gmail.com>
2023-02-07 09:13:32 -0800

    feat: set privacy settigns for firefox

--- pkgs/firefox/default.nix
+++ pkgs/firefox/default.nix
@@ -8,11 +8,23 @@
       home = {
         id = 0;
         search.default = "DuckDuckGo";
+        settings = {
+          "browser.startup.homepage" = "duckduckgo.com";
+          "privacy.clearOnShutdown.history" = true;
+          "privacy.clearOnShutdown.downloads" = true;
+          "privacy.clearOnShutdown.formdata" = true;
+        };
       };
 
       work = {
         id = 1;
         search.default = "DuckDuckGo";
+        settings = {
+          "browser.startup.homepage" = "ticktick.com";
+          "privacy.clearOnShutdown.history" = true;
+          "privacy.clearOnShutdown.downloads" = true;
+          "privacy.clearOnShutdown.formdata" = true;
+        };
       };
     };
   };
