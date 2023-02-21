ce9fbd65da979d6d52d1790e3dd5efac05dfd578
Matthew Cairns <mattrcairns@gmail.com>
2023-02-20 18:08:18 -0800

    fix: remove duplicate files

--- modules/desktop/polybar/default.nix
+++ modules/desktop/polybar/default.nix
@@ -1,11 +1,17 @@
-{
-  config,
-  pkgs,
-  ...
+{ config
+, pkgs
+, ...
 }: {
-  home.packages = with pkgs; [
-    polybarFull
-  ];
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybarFull;
+    script = ''
+      #!/usr/bin/env sh
+      for m in $(polybar --list-monitors | cut -d":" -f1); do
+        MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybar-$m.log & disown
+      done
+    '';
+  };
 
   xdg.configFile."polybar".source = ./config;
 }
