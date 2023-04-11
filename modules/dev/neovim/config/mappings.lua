1c22b2996b2aa82b3aa187256abccfa49291a4f7
Matthew Cairns <mattrcairns@gmail.com>
2023-04-11 11:08:42 -0700

    fix: change harpoon mapping to hjkl

--- modules/dev/neovim/config/mappings.lua
+++ modules/dev/neovim/config/mappings.lua
@@ -48,8 +48,8 @@ vim.api.nvim_set_keymap('n', '<leader>qp', '<cmd>:cp<CR>', { noremap = true })
 -- Harpoon
 vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua require("harpoon.mark").add_file()<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-e>', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-;>', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', { noremap = true })
 
