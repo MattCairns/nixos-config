ae744e68eaa93a28e41f00af605dfb268b915f77
Matthew Cairns <mattrcairns@gmail.com>
2023-04-17 09:20:01 -0700

    feat: add v4l2

--- config/base.nix
+++ config/base.nix
@@ -7,7 +7,18 @@
   nixpkgs.config.allowUnfree = true;
 
   # Use the latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+    kernelModules = [ "v4l2loopback" ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1
+      options v4l2loopback devices=1
+      options v4l2loopback video_nr=2
+      options v4l2loopback card_label="fake-cam"
+    '';
+  };
+
 
   nix = {
     package = pkgs.nixFlakes;
@@ -124,6 +135,7 @@
     brightnessctl
     qjackctl
     nixos-generators
+    v4l-utils
     gnome.nautilus
     (inputs.mrcoverlays.legacyPackages.x86_64-linux.aichat)
   ];
