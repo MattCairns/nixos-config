6782a691bea4d32909ad88063d18497bc24bb657
Matthew Cairns <mattrcairns@gmail.com>
2023-04-18 16:58:21 -0700

    feat: add iso file that assists with setting up persistence

--- iso.nix
+++ iso.nix
@@ -19,46 +19,79 @@
   ];
 
   environment.etc = {
-    btrfswrite = {
+    nixos-config = {
       text = ''
-        #!/usr/bin/env bash
-        cryptsetup --verify-passphrase -v luksFormat "$DISK"p3
-        cryptsetup open "$DISK"p3 enc
-        mkfs.vfat -n boot "$DISK"p1
-        mkswap "$DISK"p2
-        swapon "$DISK"p2
-        mkfs.btrfs /dev/mapper/enc
-        mount -t btrfs /dev/mapper/enc /mnt
+        { config, pkgs, ... }:
+        {
+          imports =
+            [ # Include the results of the hardware scan.
+              ./hardware-configuration.nix
+            ];
 
-        btrfs subvolume create /mnt/root
-        btrfs subvolume create /mnt/home
-        btrfs subvolume create /mnt/nix
-        btrfs subvolume create /mnt/persist
-        btrfs subvolume create /mnt/log
+          boot.kernelPackages = pkgs.linuxPackages_latest;
+          boot.supportedFilesystems = [ "btrfs" ];
+          hardware.enableAllFirmware = true;
+          nixpkgs.config.allowUnfree = true;
 
-        btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+          # Use the systemd-boot EFI boot loader.
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
 
-        umount /mnt
-        mount -o subvol=root,compress=zstd,noatime /dev/mapper/enc /mnt
+          networking.hostName = "laptop"; # Define your hostname.
+          networking.networkmanager.enable = true;
 
-        mkdir /mnt/home
-        mount -o subvol=home,compress=zstd,noatime /dev/mapper/enc /mnt/home
+          # Enable the X11 windowing system.
+          services.xserver.enable = true;
 
-        mkdir /mnt/nix
-        mount -o subvol=nix,compress=zstd,noatime /dev/mapper/enc /mnt/nix
+          # Enable the KDE Desktop Environment.
+          services.xserver.displayManager.sddm.enable = true;
+          services.xserver.desktopManager.plasma5.enable = true;
 
-        mkdir /mnt/persist
-        mount -o subvol=persist,compress=zstd,noatime /dev/mapper/enc /mnt/persist
+          # Define a user account. Don't forget to set a password with ‘passwd’.
+          users.users.matthew = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+          };
 
-        mkdir -p /mnt/var/log
-        mount -o subvol=log,compress=zstd,noatime /dev/mapper/enc /mnt/var/log
-
-        mkdir /mnt/boot
-        mount "$DISK"p1 /mnt/boot
-
+          system.stateVersion = "20.11";
+        }
       '';
 
-      mode = "0440";
+      mode = "0666";
+    };
+  };
+
+  environment.etc = {
+    btrfswrite = {
+      text = ''
+        sudo cryptsetup --verify-passphrase -v luksFormat "$DISK"p3
+        sudo cryptsetup open "$DISK"p3 enc
+        sudo mkfs.vfat -n boot "$DISK"p1
+        sudo mkswap "$DISK"p2
+        sudo swapon "$DISK"p2
+        sudo mkfs.btrfs /dev/mapper/enc
+        sudo mount -t btrfs /dev/mapper/enc /mnt
+        sudo btrfs subvolume create /mnt/root
+        sudo btrfs subvolume create /mnt/home
+        sudo btrfs subvolume create /mnt/nix
+        sudo btrfs subvolume create /mnt/persist
+        sudo btrfs subvolume create /mnt/log
+        sudo btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+        sudo umount /mnt
+        sudo mount -o subvol=root,compress=zstd,noatime /dev/mapper/enc /mnt
+        sudo mkdir /mnt/home
+        sudo mount -o subvol=home,compress=zstd,noatime /dev/mapper/enc /mnt/home
+        sudo mkdir /mnt/nix
+        sudo mount -o subvol=nix,compress=zstd,noatime /dev/mapper/enc /mnt/nix
+        sudo mkdir /mnt/persist
+        sudo mount -o subvol=persist,compress=zstd,noatime /dev/mapper/enc /mnt/persist
+        sudo mkdir -p /mnt/var/log
+        sudo mount -o subvol=log,compress=zstd,noatime /dev/mapper/enc /mnt/var/log
+        sudo mkdir /mnt/boot
+        sudo mount "$DISK"p1 /mnt/boot
+      '';
+
+      mode = "0666";
     };
   };
 }
