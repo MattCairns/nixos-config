c097240416473d50f86cce8a238a674db03b22bc
Matthew Cairns <mattrcairns@gmail.com>
2023-04-21 16:06:36 -0700

    feat: add cachix to cache-runner

--- machines/cache-runner/configuration.nix
+++ machines/cache-runner/configuration.nix
@@ -83,11 +83,6 @@
     };
   };
 
-  swapDevices = [{
-    device = "/var/lib/swapfile";
-    size = 16 * 1024;
-  }];
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
