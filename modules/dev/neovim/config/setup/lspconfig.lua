2d0afd31d83373c3e3be3b9a65f074efb28a9de2
Matthew Cairns <git@cairns.pro>
2023-05-12 08:57:20 -0700

    fix: Error icon in lsp

--- modules/dev/neovim/config/setup/lspconfig.lua
+++ modules/dev/neovim/config/setup/lspconfig.lua
@@ -17,7 +17,7 @@ require'lspconfig'.pyright.setup{
   }   
 }
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
