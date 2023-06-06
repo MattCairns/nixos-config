89e6f869af2c9ce957d7555b1c03f2af21df2532
Matthew Cairns <git@cairns.pro>
2023-06-05 23:32:26 -0700

    feat: add neomake and configure

--- modules/dev/neovim/config/mappings.lua
+++ modules/dev/neovim/config/mappings.lua
@@ -52,3 +52,17 @@ vim.api.nvim_set_keymap('n', '<leader>he', '<cmd>ClangdSwitchSourceHeader<cr>', 
 -- Neoai
 vim.api.nvim_set_keymap('n', '<leader>na', '<cmd>NeoAI<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>nc', '<cmd>NeoAIContext<cr>', { noremap = true })
+
+-- Neomake
+vim.cmd([[
+" When writing a buffer (no delay).
+call neomake#configure#automake('w')
+" When writing a buffer (no delay), and on normal mode changes (after 750ms).
+call neomake#configure#automake('nw', 750)
+" When reading a buffer (after 1s), and when writing (no delay).
+call neomake#configure#automake('rw', 1000)
+" Full config: when writing or reading a buffer, and on changes in insert and
+" normal mode (after 500ms; no delay when writing).
+call neomake#configure#automake('nrwi', 500)
+let g:neomake_open_list = 2
+]])
