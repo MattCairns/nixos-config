e93522f1be27d6a86a0e311c542a8e6819135a34
Matthew Cairns <git@cairns.pro>
2023-06-12 09:25:07 -0700

    feat: add noice

--- modules/dev/neovim/default.nix
+++ modules/dev/neovim/default.nix
@@ -1,5 +1,4 @@
 { pkgs
-, test-pkgs
 , lib
 , ...
 }:
@@ -145,6 +144,32 @@ in
         }
 
         pkgs.vimPlugins.surround-nvim
+
+        {
+          plugin = pkgs.vimPlugins.noice-nvim;
+          config = ''
+            require("noice").setup({
+              lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                  ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                  ["vim.lsp.util.stylize_markdown"] = true,
+                  ["cmp.entry.get_documentation"] = true,
+                },
+              },
+              -- you can enable a preset for easier configuration
+              presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+              },
+            })
+          '';
+          type = "lua";
+        }
+
       ];
 
       extraLuaConfig = ''
