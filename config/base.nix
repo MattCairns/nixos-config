1374a0f83bc21fedcbb9a8ccbfa3f1c73309cd3b
matthew cairns <matthew@cairns.pro>
2023-10-26 19:43:40 -0700

    build: update nix flake

--- config/base.nix
+++ config/base.nix
@@ -47,11 +47,13 @@
         "https://cache.nixos.org"
         "https://cuda-maintainers.cachix.org"
         "https://hyprland.cachix.org"
+        "https://mattcairns-cachix.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "mattcairns-cachix.cachix.org-1:bl0XYmyFCxApUSk4Eo9xAqjI7HeWBym1arunM4hLvHQ="
       ];
     };
   };
