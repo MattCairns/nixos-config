aa4a14153ea5563b2235d8473f4cf49cacf34e43
matthew cairns <matthew@cairns.pro>
2023-11-04 10:19:46 -0700

    feat: more hyprland setup (swayidle, swaylock)

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -21,27 +21,6 @@
     package = pkgs.swaylock-effects;
   };
 
-  services.swayidle = {
-    enable = true;
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.swaylock}/bin/swaylock";
-      }
-      {
-        event = "lock";
-        command = "lock";
-      }
-    ];
-
-    timeouts = [
-      {
-        timeout = 60;
-        command = "${pkgs.swaylock}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 3 --effect-blur 8x10 --effect-vignette 0.5:0.5 --ring-color 1a1b26 --key-hl-color 96be25 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --fade-in 1";
-      }
-    ];
-  };
-
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = true;
@@ -52,4 +31,6 @@
   # xdg.configFile."hypr/hyprland.conf".source = ./config/hyprland.conf;
   xdg.configFile."hypr/machine.conf".source = ./config/${machine}.conf;
   xdg.configFile."hypr/hyprpaper.conf".source = ./config/hyprpaper.conf;
+  xdg.configFile."hypr/start-apps.sh".source = ./config/start-apps.sh;
+  xdg.configFile."hypr/start-swaylock.sh".source = ./config/start-swaylock.sh;
 }
