ef1c4912c6fb3d923b550a9672758af7daf10ac2
matthew cairns <matthew@cairns.pro>
2023-11-16 22:06:17 -0800

    feat: replace rust-tools with rustaceanvim

--- modules/dev/neovim/default.nix
+++ modules/dev/neovim/default.nix
@@ -169,51 +169,8 @@ in {
           config = "require('crates').setup{}";
           type = "lua";
         }
-        {
-          plugin = pkgs.vimPlugins.rust-tools-nvim;
-          config = ''
-            local rt = require("rust-tools")
-            rt.setup({
-              server = {
-                on_attach = function(_, bufnr)
-                  vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                  vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-                end,
-              settings = {
-                ["rust-analyzer"] = {
-                  assist = {
-                    importEnforceGranularity = true,
-                    importPrefix = "crate"
-                    },
-                  cargo = {
-                    allFeatures = true
-                    },
-                  checkOnSave = {
-                    -- default: `cargo check`
-                    command = "clippy"
-                    },
-                  },
-                  inlayHints = {
-                    lifetimeElisionHints = {
-                      enable = true,
-                      useParameterNames = true
-                    },
-                  },
-                }
-              },
-              tools = {
-                dap = {
-                  adapter = {
-                    id = 'cppdbg',
-                    type = 'executable',
-                    command = dirLookup('/nix/store/*vscode-extension-ms-vscode-cpptools*')..'/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7',
-                  },
-                },
-              },
-            })
-          '';
-          type = "lua";
-        }
+        pkgs.vimPlugins.rustaceanvim
+
       ];
 
       extraLuaConfig = ''
