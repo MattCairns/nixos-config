c4c63992cbb5d8114eefef38ae50098dc006bf90
matthew cairns <matthew@cairns.pro>
2023-11-20 22:32:13 -0800

    feat: use swww to select random wallpaper

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -12,6 +12,7 @@
     wofi
     wl-clipboard
     dunst
+    swww
   ];
 
   programs.wlogout = {
@@ -32,7 +33,7 @@
 
   # xdg.configFile."hypr/hyprland.conf".source = ./config/hyprland.conf;
   xdg.configFile."hypr/machine.conf".source = ./config/${machine}.conf;
-  xdg.configFile."hypr/hyprpaper.conf".source = ./config/hyprpaper.conf;
+  # xdg.configFile."hypr/hyprpaper.conf".source = ./config/hyprpaper.conf;
   xdg.configFile."hypr/start-apps.sh".source = ./config/start-apps.sh;
   xdg.configFile."hypr/start-swaylock.sh".source = ./config/start-swaylock.sh;
   xdg.configFile."hypr/toggle-tailscale.sh".source = ./config/toggle-tailscale.sh;
