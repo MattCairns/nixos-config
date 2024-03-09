dca1a4513e94c00696d3beb160b49807a35df493
Matthew Cairns <git@cairns.pro>
2024-03-09 15:24:37 +0000

    chore: Update flake lockfile

--- config/base.nix
+++ config/base.nix
@@ -160,13 +160,13 @@
     user = "${user}";
   };
 
-  # Set XDG environment
   environment.sessionVariables = {
     XDG_CONFIG_HOME = "\${HOME}/.config";
     XDG_CACHE_HOME = "\${HOME}/.local/cache";
     XDG_BIN_HOME = "\${HOME}/.local/bin";
     XDG_DATA_HOME = "\${HOME}/.local/share";
     PATH = ["\${XDG_BIN_HOME}"];
+    EDITOR = "nvim";
     XCURSOR_SIZE = "32";
   };
 
