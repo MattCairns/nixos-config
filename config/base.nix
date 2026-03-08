f3cf5cc629cedf7e314f86b3e708f8dd1c9799aa
Matthew Cairns <git@cairns.pro>
2026-03-08 09:33:31 -0700

    fix: Formatting

--- config/base.nix
+++ config/base.nix
@@ -5,8 +5,7 @@
   lib,
   inputs,
   ...
-}:
-let
+}: let
   rnnoise_config = {
     "context.modules" = [
       {
@@ -63,10 +62,9 @@ let
       '';
     }
     // {
-      providedSessions = [ "bspwm" ];
+      providedSessions = ["bspwm"];
     };
-in
-{
+in {
   imports = [
     inputs.talon-nix.nixosModules.talon
   ];
@@ -76,8 +74,7 @@ in
     allowUnfree = true;
 
     # Explicitly set which non-free packages can be installed
-    allowUnfreePredicate =
-      pkg:
+    allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) [
         "codeium"
         "discord"
@@ -129,7 +126,7 @@ in
 
   # udev rules
   services.udev = {
-    packages = [ pkgs.qmk-udev-rules ];
+    packages = [pkgs.qmk-udev-rules];
     extraRules = ''
       SUBSYSTEM=="tty", ATTRS{product}=="CubeOrange", SYMLINK="ttyPIXHAWK"
     '';
@@ -144,7 +141,7 @@ in
     grub.efiSupport = false;
     grub.device = "nodev";
   };
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -157,7 +154,7 @@ in
       5000
       51820
     ];
-    allowedTCPPorts = [ 14557 ];
+    allowedTCPPorts = [4096 14557];
   };
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
@@ -172,7 +169,7 @@ in
 
   services.xserver.enable = true;
 
-  services.displayManager.sessionPackages = [ bspwmSession ];
+  services.displayManager.sessionPackages = [bspwmSession];
 
   programs.niri.enable = true;
   # Prevent niri's gnome portal from overriding the ssh-agent
@@ -196,8 +193,7 @@ in
   # Explicitly set XDG_DATA_DIRS in greetd's systemd environment so regreet
   # can discover session .desktop files. PAM DEFAULT= won't reliably propagate
   # to the greeter child process without this.
-  systemd.services.greetd.environment.XDG_DATA_DIRS =
-    "${config.services.displayManager.sessionData.desktops}/share";
+  systemd.services.greetd.environment.XDG_DATA_DIRS = "${config.services.displayManager.sessionData.desktops}/share";
 
   xdg.portal = {
     enable = true;
@@ -234,12 +230,12 @@ in
   # Enable CUPS to print documents.
   services.printing = {
     enable = true;
-    drivers = [ pkgs.hplip ];
+    drivers = [pkgs.hplip];
   };
 
   programs.gnupg.agent.enable = true;
   security.polkit.enable = true;
-  security.pam.services.swaylock = { };
+  security.pam.services.swaylock = {};
 
   security.sudo = {
     enable = true;
@@ -248,24 +244,24 @@ in
         commands = [
           {
             command = "/run/current-system/sw/bin/nixos-rebuild";
-            options = [ "NOPASSWD" ];
+            options = ["NOPASSWD"];
           }
         ];
-        users = [ "${user}" ];
+        users = ["${user}"];
       }
       {
         commands = [
           {
             command = "${pkgs.tailscale}/bin/tailscale";
-            options = [ "NOPASSWD" ];
+            options = ["NOPASSWD"];
           }
         ];
-        groups = [ "wheel" ];
+        groups = ["wheel"];
       }
     ];
   };
 
-  services.dbus.packages = [ pkgs.gcr ];
+  services.dbus.packages = [pkgs.gcr];
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -279,7 +275,7 @@ in
     jack.enable = true;
     extraConfig.pipewire."99-input-denoising" = rnnoise_config;
   };
-  users.extraGroups.audio.members = [ "${user}" ];
+  users.extraGroups.audio.members = ["${user}"];
 
   # Enable syncthing
   services.syncthing = {
@@ -295,7 +291,7 @@ in
     XDG_CACHE_HOME = "\${HOME}/.local/cache";
     XDG_BIN_HOME = "\${HOME}/.local/bin";
     XDG_DATA_HOME = "\${HOME}/.local/share";
-    PATH = [ "\${XDG_BIN_HOME}" ];
+    PATH = ["\${XDG_BIN_HOME}"];
     EDITOR = "nvim";
     XCURSOR_SIZE = "32";
     NH_FLAKE = "\${HOME}/nixos-config";
@@ -334,11 +330,11 @@ in
   ];
 
   # Audio firmware and hardware support
-  hardware.firmware = [ pkgs.linux-firmware ];
+  hardware.firmware = [pkgs.linux-firmware];
   hardware.enableRedistributableFirmware = true;
 
   virtualisation.docker.enable = true;
-  users.extraGroups.docker.members = [ "${user}" ];
+  users.extraGroups.docker.members = ["${user}"];
 
   # Set up shell
   users.defaultUserShell = pkgs.fish;
